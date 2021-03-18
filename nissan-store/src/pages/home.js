1d0eeeb900ee363c346cd259eb67e1012d91ffdd
cardugarte <contacto@carlosdugarte.com>
2021-03-18 01:18:07 -0300

    Configuración inicial del proyecto

---
+++ nissan-store/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Ahora es online</h1>
+      <p>Con Nissan Store podés acceder a tu nuevo 0km con todos los servicios asociados desde la comodidad de tu sofá</p>
+      <span>Probanos!</span>
+    </div>
+  );
+}
+
+export default Home;
